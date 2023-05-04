cc09ea3fe0c8ebf287909e00296662d1baf6604e
David_Moura <davidmoura0102@gmail.com>
2023-05-04 13:11:24 -0300

    logout de usuários

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('logout/', views.logout_user, name='logout')
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.shortcuts import redirect
 
@@ -47,7 +47,9 @@ def cadastro(request):
 
         return redirect(reverse('login'))
         
-        
+def logout_user(request):
+    logout(request)
+    return redirect(reverse('login'))
         
 
         
