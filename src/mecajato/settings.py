532d249f02c059d22eaa65bfc8c04c7483713d3d
David_Moura <davidmoura0102@gmail.com>
2023-05-13 00:01:29 -0300

    .env

--- src/mecajato/settings.py
+++ src/mecajato/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,10 +84,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mecajato',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'USER': os.environ['user'],
+        'PASSWORD': os.environ['password'],
+        'HOST': os.environ['host'],
+        'PORT': os.environ['port'],
     }
 }
 
